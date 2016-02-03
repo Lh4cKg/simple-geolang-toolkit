4cb815274f19dcb2cd69e4d1b0beaefd568d93f9
Lasha Gogua <lhackg@yandex.com>
2016-02-03 05:17:05 +0400

    update geolang toolkit package

--- geolang/geolang.py
+++ geolang/geolang.py
@@ -15,20 +15,20 @@ import re
 from django.template.defaultfilters import slugify
 
 # KA_GE = 'აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ'
-# LATIN = 'abgdevzTiklmnopJrstufqRySCcfwWxjh'
+# LATIN = 'abgdevzTiklmnopJrstufqRySCcZwWxjh'
 
 # character map of georgian to latin
 """
 {'ხ': 'x', 'ა': 'a', 'ჭ': 'W', 'ჟ': 'J', 'ბ': 'b', 'რ': 'r', 'დ': 'd', 'ნ': 'n', 'ჩ': 'C', 'ფ': 'f', 'უ': 'u', 'თ': 'T', 'პ': 'p', 
   'ტ': 't', 'ზ': 'z', 'ი': 'i', 'ლ': 'l', 'წ': 'w', 'გ': 'g', 'ღ': 'R', 'ე': 'e', 'მ': 'm', 'ყ': 'y', 'ვ': 'v', 'შ': 'S', 'ჰ': 'h', 
-  'კ': 'k', 'ძ': 'f', 'ქ': 'q', 'ო': 'o', 'ჯ': 'j', 'ც': 'c', 'ს': 's'}
+  'კ': 'k', 'ძ': 'Z', 'ქ': 'q', 'ო': 'o', 'ჯ': 'j', 'ც': 'c', 'ს': 's'}
 """
 # KA2LAT = {c: i for i, c in zip(LATIN, KA_GE)}
 
 # character map of latin to georgian
 """
 {'S': 'შ', 'W': 'ჭ', 'k': 'კ', 'u': 'უ', 'n': 'ნ', 'R': 'ღ', 'o': 'ო', 'a': 'ა', 'b': 'ბ', 'v': 'ვ', 'x': 'ხ', 'j': 'ჯ', 'p': 'პ',
-  'C': 'ჩ', 't': 'ტ', 'J': 'ჟ', 's': 'ს', 'l': 'ლ', 'r': 'რ', 'f': 'ძ', 'm': 'მ', 'i': 'ი', 'h': 'ჰ', 'q': 'ქ', 'e': 'ე', 'T': 'თ', 
+  'C': 'ჩ', 't': 'ტ', 'J': 'ჟ', 's': 'ს', 'l': 'ლ', 'r': 'რ', 'Z': 'ძ', 'm': 'მ', 'i': 'ი', 'h': 'ჰ', 'q': 'ქ', 'e': 'ე', 'T': 'თ', 
   'c': 'ც', 'd': 'დ', 'z': 'ზ', 'g': 'გ', 'w': 'წ', 'y': 'ყ'}
 """
 # LAT2KA = {c: i for i, c in zip(KA_GE, LATIN)}
@@ -52,99 +52,100 @@ from django.template.defaultfilters import slugify
 
 # print(_2KA('data'))
 
+
+
 class GeoLangToolKit(object):
+	def __init__(self):
+		"""
+		Desc: georgian and latin alphabet
+		ანბანში დამატებითი ასოები აღნიშნულია, შემდეგი ასოებით:
+			თ - T
+			ჟ - J
+			ღ - R
+			შ - S
+			ჩ - C
+			ძ - Z
+			ჭ - W
+		"""
 
-        def __init__(self):
-                """
-                Desc: georgian and latin alphabet
-                ანბანში დამატებითი ასოები აღნიშნულია, შემდეგი ასოებით:
-                    თ - T
-                    ჟ - J
-                    ღ -R
-                    შ - S
-                    ჩ - C
-                    ძ - Z
-                    ჭ - W
-                """
+		self.KA_GE = 'აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ'
+		self.LATIN = 'abgdevzTiklmnopJrstufqRySCcZwWxjh'
+		self.LAT_ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
 
-                self.KA_GE = 'აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ'
-                self.LATIN = 'abgdevzTiklmnopJrstufqRySCcZwWxjh'
-                self.LAT_ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+	@property
+	def KA2LAT(self):
+		"""
+		    Desc: character map of georgian to latin
 
-        @property
-        def KA2LAT(self):
-                """
-                    Desc: character map of georgian to latin
+		    >>> KA2LAT()
+		    {'ხ': 'x', 'ა': 'a', 'ჭ': 'W', 'ჟ': 'J', 'ბ': 'b', 'რ': 'r', 'დ': 'd', 'ნ': 'n', 'ჩ': 'C', 'ფ': 'f', 'უ': 'u', 'თ': 'T', 'პ': 'p', 
+		      'ტ': 't', 'ზ': 'z', 'ი': 'i', 'ლ': 'l', 'წ': 'w', 'გ': 'g', 'ღ': 'R', 'ე': 'e', 'მ': 'm', 'ყ': 'y', 'ვ': 'v', 'შ': 'S', 'ჰ': 'h', 
+		      'კ': 'k', 'ძ': 'Z', 'ქ': 'q', 'ო': 'o', 'ჯ': 'j', 'ც': 'c', 'ს': 's'}
+		"""
 
-                    >>> KA2LAT()
-                    {'ხ': 'x', 'ა': 'a', 'ჭ': 'W', 'ჟ': 'J', 'ბ': 'b', 'რ': 'r', 'დ': 'd', 'ნ': 'n', 'ჩ': 'C', 'ფ': 'f', 'უ': 'u', 'თ': 'T', 'პ': 'p', 
-                      'ტ': 't', 'ზ': 'z', 'ი': 'i', 'ლ': 'l', 'წ': 'w', 'გ': 'g', 'ღ': 'R', 'ე': 'e', 'მ': 'm', 'ყ': 'y', 'ვ': 'v', 'შ': 'S', 'ჰ': 'h', 
-                      'კ': 'k', 'ძ': 'Z', 'ქ': 'q', 'ო': 'o', 'ჯ': 'j', 'ც': 'c', 'ს': 's'}
-                """
+		convert = {c: i for i, c in zip(self.KA_GE,self.LATIN)}
+		return convert
 
-                convert = {c: i for i, c in zip(self.KA_GE,self.LATIN)}
-                return convert
+	@property
+	def LAT2KA(self):
+		"""
+		    Desc: character map of latin to georgian
+		    >>> LAT2KA()
+		    {'S': 'შ', 'W': 'ჭ', 'k': 'კ', 'u': 'უ', 'n': 'ნ', 'R': 'ღ', 'o': 'ო', 'a': 'ა', 'b': 'ბ', 'v': 'ვ', 'x': 'ხ', 'j': 'ჯ', 'p': 'პ',
+		      'C': 'ჩ', 't': 'ტ', 'J': 'ჟ', 's': 'ს', 'l': 'ლ', 'r': 'რ', 'Z': 'ძ', 'm': 'მ', 'i': 'ი', 'h': 'ჰ', 'q': 'ქ', 'e': 'ე', 'T': 'თ', 
+		      'c': 'ც', 'd': 'დ', 'z': 'ზ', 'g': 'გ', 'w': 'წ', 'y': 'ყ'}
+		"""
 
-        @property
-        def LAT2KA(self):
-                """
-                    Desc: character map of latin to georgian
-                    >>> LAT2KA()
-                    {'S': 'შ', 'W': 'ჭ', 'k': 'კ', 'u': 'უ', 'n': 'ნ', 'R': 'ღ', 'o': 'ო', 'a': 'ა', 'b': 'ბ', 'v': 'ვ', 'x': 'ხ', 'j': 'ჯ', 'p': 'პ',
-                      'C': 'ჩ', 't': 'ტ', 'J': 'ჟ', 's': 'ს', 'l': 'ლ', 'r': 'რ', 'Z': 'ძ', 'm': 'მ', 'i': 'ი', 'h': 'ჰ', 'q': 'ქ', 'e': 'ე', 'T': 'თ', 
-                      'c': 'ც', 'd': 'დ', 'z': 'ზ', 'g': 'გ', 'w': 'წ', 'y': 'ყ'}
-                """
+		convert = {c: i for i, c in zip(self.LATIN, self.KA_GE)}
+		return convert
 
-                convert = {c: i for i, c in zip(self.LATIN, self.KA_GE)}
-                return convert
+	def _2KA(self,data):
+		"""
+		Desc: convert the given name from latin into georgian chars
 
-        def _2KA(self,data):
-                """
-                     Desc: convert the given name from latin into georgian chars
+		>>> _2KA('laSas uyvars ani da piToni lol ))')
+		ლაშას უყვარს ანი და პითონი ლოლ
+		"""
 
-                     >>> _2KA('laSas uyvars ani da piToni lol ))')
-                     ლაშას უყვარს ანი და პითონი ლოლ
-                """
+		converted = []
+		_KAchars = self.KA2LAT.keys()
+		i = 0
+		while i < len(data.lower()):
+			char = data[i]
+			i += 1
+			try:
+				converted.append(self.KA2LAT[char])
+			except KeyError:
+				if char in _KAchars:
+					converted.append(char)
+				else:
+					converted.append(' ')
 
-                converted = []
-                _KAchars = self.KA2LAT.keys()
-                i = 0
-                while i < len(data.lower()):
-                        char = data[i]
-                        i += 1
-                        try:
-                                converted.append(self.KA2LAT[char])
-                        except KeyError:
-                                if char in _KAchars:
-                                        converted.append(char)
-                                else:
-                                        converted.append(' ')
+		return ''.join(converted)
 
-                return ''.join(converted)
+	def _2LAT(self,data):
+		"""
+		Desc: convert the given name from georgian into latin chars
 
-        def _2LAT(self,data):
-                """
-                Desc: convert the given name from georgian into latin chars
+		>>> _2LAT('მე მიყვარს ანი, ის ცხოვრობს თბილიში!')
+		me miyvars ani  is cxovrobs TbiliSi
+		"""
 
-                >>> _2LAT('მე მიყვარს ანი, ის ცხოვრობს თბილიში!')
-                me miyvars ani  is cxovrobs TbiliSi
-                """
+		converted = []
+		_LATchars = self.KA2LAT.keys()
+		i = 0
+		while i < len(data.lower()):
+			char = data[i]
+			i += 1
+			try:
+				converted.append(self.LAT2KA[char])
+			except KeyError:
+				if char in _LATchars:
+					converted.append(char)
+				else:
+					converted.append(' ')
 
-                converted = []
-                _LATchars = self.KA2LAT.keys()
-                i = 0
-                while i < len(data.lower()):
-                        char = data[i]
-                        i += 1
-                        try:
-                                converted.append(self.LAT2KA[char])
-                        except KeyError:
-                                if char in _LATchars:
-                                        converted.append(char)
-                                else:
-                                        converted.append(' ')
-
-                return ''.join(converted)
+		return ''.join(converted)
 
 _inst = GeoLangToolKit()
 K2L = _inst.KA2LAT
