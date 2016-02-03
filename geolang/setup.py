77ea531f36bd1cbdc8a877e683f641f9129a2ecb
Lasha Gogua <lhackg@yandex.com>
2016-02-03 22:32:37 +0400

    added setup.py file for installation as Python Modules

---
+++ geolang/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import sys
+from distutils.core import setup
+from geolang import __version__, __author__
+assert sys.version >= "3.0", "Requires Python v3.0 or above versions."
+
+setup (
+	name = "geolang",
+	version = __version__,
+	description = "Simple Georgian Language ToolKit",
+	author = __author__,
+	author_email = "Lh4cKg@gmail.com",
+	url = "https://github.com/Lh4cKg/simple-geolang-toolkit/",
+	packages = ["geolang"],
+	classifiers=[
+		"Development Status :: Beta",
+		"Environment :: Console",
+		"Environment :: Web Environment",
+		"License :: OSI Approved :: Python Software Foundation License",
+		"Operating System :: POSIX :: Linux",
+		"Operating System :: MacOS :: MacOS X",
+		"Operating System :: Microsoft :: Windows",
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 3.0",
+		"Programming Language :: Python :: 3.1",
+		"Programming Language :: Python :: 3.2",
+		"Programming Language :: Python :: 3.3",
+		"Programming Language :: Python :: 3.4",
+		"Programming Language :: Python :: 3.5",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+          ],
+)
