2e713f4f2f5cc206c5029bb00db0fa10f428fafc
Lasha Gogua <lhackg@yandex.com>
2016-02-03 20:32:07 +0400

    Initialize the geolang toolkit package

---
+++ geolang/__init__.py
@@ -0,0 +1,14 @@
+from geolang.geolang import (
+					__author__,
+					__version__,
+					KA2LAT,
+					LAT2KA,
+					UNI2LAT,
+					_2KA,
+					_2LAT,
+					encode_slugify,
+					GeoLangToolKit,
+					unicode,
+				)
+# from geolang.geolang import *
+from .uni2lat import *
