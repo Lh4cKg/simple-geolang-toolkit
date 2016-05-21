73a2b25a5989e23f73b0597ebeddf5db788747fa
Lh4cKg <lhackg@yandex.com>
2016-05-22 01:21:40 +0400

    update __init__

--- geolang/__init__.py
+++ geolang/__init__.py
@@ -1,17 +1,16 @@
+"""
+ initialization package imports
+"""
+
 from geolang.geolang import (
-					__author__,
-					__version__,
-					_KA_ALPHABET,
-					_LAT_ALPHABET,
-					KA2KA,
-					LAT2LAT,
-					KA2LAT,
-					LAT2KA,
-					UNI2LAT,
-					_2KA,
-					_2LAT,
-					encode_slugify,
-					GeoLangToolKit,
-				)
+    __author__,
+    __version__,
+    ka2Lat,
+    lat2Ka,
+    _2ka,
+    _2lat,
+    encode_slugify,
+    GeoLangToolKit,
+)
 # from geolang.geolang import *
-from .unicode import unicode
+from .unicode import unicode_all, unicode_ka
